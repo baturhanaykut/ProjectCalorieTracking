8eedf0751a7fdc4819f1c8c2115f61dff4ea2e73
Baturhan Aykut <baturhanaykut@gmail.com>
2023-01-24 23:39:26 +0300

    Son Hali

--- UI/frmStatistics.cs
+++ UI/frmStatistics.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
@@ -28,35 +29,202 @@ namespace UI
         private void StatisticsForm_Load(object sender, EventArgs e)
         {
             context = new CalorieTrackingContext();
-
-            cmbChooseMeal.Items.Clear();
-            cmbChooseMeal.DataSource = Enum.GetNames(typeof(MealType)).ToArray();
-
-            var result = (from f in context.Foods
-                join fm in context.Meals on fm 
-
-            dgvChooseFood.DataSource = result;
-
-            //cmbChooseMeal.DataSource = context.Categories.Select(c => c.CategoryName).ToList();
-            //cmbChooseMeal.SelectedIndex = -1;
+            
+            cmbChooseCategory.DataSource = context.Categories.Select(c => c.CategoryName).ToList();
+            cmbChooseCategory.SelectedIndex = -1;
         }
         private void btnBackUserForm_Click(object sender, EventArgs e)
         {
             _frmUserProfile.Show();
             this.Close();
         }
-        private void cmbChooseMeal_SelectedIndexChanged(object sender, EventArgs e)
+        
+        private void cmbChooseCategory_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (cmbChooseCategory.SelectedIndex == 0)
+            {
+                MealsEatenByCategory();
+                MostEatenMeal();
+            }
+            else if (cmbChooseCategory.SelectedIndex == 1)
+            {
+                MealsEatenByCategory();
+                MostEatenMeal();
+
+            }
+            else if (cmbChooseCategory.SelectedIndex == 2)
+            {
+                MealsEatenByCategory();
+                MostEatenMeal();
+            }
+            else if (cmbChooseCategory.SelectedIndex == 3)
+            {
+                MealsEatenByCategory();
+                MostEatenMeal();
+            }
 
         }
 
+        //private void rbtnBreakfast_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    MostEatenMeal();
+        //}
+        private void rbtnBreakfast_Click(object sender, EventArgs e)
+        {
+            MostEatenMeal();
+        }
 
-        //Hangi yemeklerin hangi öğünlerde ne kadar yendiğini gösteren bir rapor hazırlansın.
-        //En çok yenen yemekler raporu çıksın.
+        private void MostEatenMeal()
+        {
+            if (rbtnBreakfast.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Breakfast)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName, a.Category.CategoryID })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).OrderBy(x => x.Count).ToList();
+
+                
+                dgvMealReport.DataSource = listGroupBy;
+
+            //    dgvMealReport.Columns[0].DataPropertyName = "Key";
+            //    dgvMealReport.Columns[0].HeaderText = "Food Name";
+            //    dgvMealReport.Columns[1].DataPropertyName = "Sum";
+            //    dgvMealReport.Columns[1].HeaderText = "Total Calorie";
+            //    dgvMealReport.Columns[2].DataPropertyName = "Count";
+            //    dgvMealReport.Columns[2].HeaderText = "Total Porsion";
+            }
+            else if (rbtnDinner.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Dinner)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).FirstOrDefault();
+
+                dgvMealReport.DataSource = listGroupBy;
 
 
+            }
 
+            else if (rbtnSnack.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Snack)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).FirstOrDefault();
+
+                dgvMealReport.DataSource = listGroupBy;
+
+
+            }
+            else if (rbtnLunch.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Lunch)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).FirstOrDefault();
+
+                dgvMealReport.DataSource = listGroupBy;
+
+            }
+        }
+
+
+        private void MealsEatenByCategory()
+        {
+            if (rbtnBreakfast.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Breakfast)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName, a.Category.CategoryID })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).OrderBy(x=>x.Count) .ToList();
+
+                dgvChooseFood.DataSource = listGroupBy;
+
+                dgvChooseFood.Columns[0].DataPropertyName = "Key";
+                dgvChooseFood.Columns[0].HeaderText = "Food Name";
+                dgvChooseFood.Columns[1].DataPropertyName = "Sum";
+                dgvChooseFood.Columns[1].HeaderText = "Total Calorie";
+                dgvChooseFood.Columns[2].DataPropertyName = "Count";
+                dgvChooseFood.Columns[2].HeaderText = "Total Porsion";
+            }
+            else if (rbtnDinner.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Dinner)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).ToList();
+
+                dgvChooseFood.DataSource = listGroupBy;
+
+                
+            }
+
+            else if (rbtnSnack.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Snack)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).ToList();
+
+                dgvChooseFood.DataSource = listGroupBy;
+
+                
+            }
+            else if (rbtnLunch.Checked == true)
+            {
+                var list = context.Meals
+                    .Where(m => m.MealName == MealType.Lunch)
+                    .SelectMany(f => f.Foods)
+                    .Select(a => new { a.FoodName, a.Porsion, a.Calorie, a.Category.CategoryName })
+                    .Where(b => b.CategoryName == cmbChooseCategory.Text)
+                    .ToList();
+
+                var listGroupBy = list.GroupBy(x => x.FoodName)
+                    .Select(b => new { b.Key, Sum = b.Sum(s => s.Calorie), Count = b.Count() }).ToList();
+
+                dgvChooseFood.DataSource = listGroupBy;
+
+            }
+        }
 
     }
+
+   
+
+    
 }
